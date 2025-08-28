b5297b9a062fe35a59491845575ec7361db6a2e6
owusuvictorjnr <owusuvictor80@gmail.com>
2025-08-28 08:46:27 +0000

    add query provider to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { QueryProvider } from "@/context/QueryProvider";
+import { ErrorProvider } from "@/context/ErrorProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <QueryProvider>
+          {/* <ZustandProvider> */}
+          <ErrorProvider>{children}</ErrorProvider>
+          {/* </ZustandProvider> */}
+        </QueryProvider>
       </body>
     </html>
   );
