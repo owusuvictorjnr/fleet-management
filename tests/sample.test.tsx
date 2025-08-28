1c13e9230c3472af47a6fda9af3f4b782d1fbf55
owusuvictorjnr <owusuvictor80@gmail.com>
2025-08-28 08:49:00 +0000

    renamed

--- tests/sample.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Home from "../app/page";
-
-test("renders homepage", () => {
-  render(<Home />);
-  expect(screen.getByText(/Welcome/i)).toBeInTheDocument();
-});
