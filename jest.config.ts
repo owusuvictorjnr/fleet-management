04670129ab4dd59a89d355992fb034baee6ec03f
owusuvictorjnr <owusuvictor80@gmail.com>
2025-08-28 08:44:43 +0000

    test config file

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('jest').Config} */
+const config: import('jest').Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  transform: {
+    "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "tsconfig.json" }],
+  },
+};
+
+module.exports = config;
