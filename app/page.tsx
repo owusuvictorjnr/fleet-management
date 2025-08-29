d0e8935e75f9253771e4ab43c17f88cbc62457f6
owusuvictorjnr <owusuvictor80@gmail.com>
2025-08-29 10:54:27 +0000

    use section in replace of main

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
-import { Card, CardContent } from "@/components/ui/card";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-50">
-      <Card className="w-[300px] p-4 shadow-md">
-        <CardContent className="">
-          <h1 className="text-xl font-bold mb-4">Welcome to Battery Tracker</h1>
-        </CardContent>
-      </Card>
-    </main>
+    <section className="text-center">
+      <h1 className="text-3xl font-bold mb-4">
+        Welcome to <span className="text-blue-600">Battery Tracker</span>{" "}
+      </h1>
+
+      <p className="text-gray-600 max-w-xl mx-auto mb-6">
+        Track your battery performance, health, and history with ease.
+      </p>
+
+      <div className="flex justify-center gap-4">
+        <a
+          href="/dashboard"
+          className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition"
+        >
+          Go to Dashboard
+        </a>
+        <a
+          href="/auth/login"
+          className="  px-6 py-3 border border-gray-300 rounded-lg shadow hover:bg-gray-100 transition"
+        >
+          Login
+        </a>
+      </div>
+    </section>
   );
 }
